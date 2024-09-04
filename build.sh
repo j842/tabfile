24210b7dca82815847b1778e14ba98ca011ce709
j842 <j@842.be>
2024-09-04 19:57:04 +1200

    Fix build to be able to be run from other directories.

--- build.sh
+++ build.sh
@@ -6,8 +6,8 @@ USERID=$(id -u)
 GROUPID=$(id -g)
 
 echo "Building docker image"
-docker build -t tabbuild .
+docker build -t tabbuild "${SCRIPT_DIR}"
 
 echo "Building tabfile"
-docker run -v "$(pwd)":/root tabbuild /bin/bash -c "cd /root && make && mv build/tabfile . && rm -rf build && chown ${USERID}:${GROUPID} tabfile"
+docker run -v "${SCRIPT_DIR}":/root tabbuild /bin/bash -c "cd /root && make && mv build/tabfile . && rm -rf build && chown ${USERID}:${GROUPID} tabfile"
 
