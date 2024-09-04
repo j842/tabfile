bf7911a47d099fcc1e2da700339569b6235711ba
j842 <j@842.be>
2024-09-04 19:54:32 +1200

    Fix group id

--- build.sh
+++ build.sh
@@ -2,9 +2,11 @@
 
 SCRIPT_DIR="$(dirname "$(realpath "$0")")"
 
+GROUPID=$(id -g)
+
 echo "Building docker image"
 docker build -t tabbuild .
 
 echo "Building tabfile"
-docker run -v "$(pwd)":/root tabbuild /bin/bash -c "cd /root && make && mv build/tabfile . && rm -rf build && chown ${UID}:${UID} tabfile"
+docker run -v "$(pwd)":/root tabbuild /bin/bash -c "cd /root && make && mv build/tabfile . && rm -rf build && chown ${UID}:${GROUPID} tabfile"
 
