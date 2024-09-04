d40a39f09e0ab26136e580e8c82d42d180c0a1e3
j842 <j@842.be>
2024-09-04 19:55:55 +1200

    TIdy

--- build.sh
+++ build.sh
@@ -2,11 +2,12 @@
 
 SCRIPT_DIR="$(dirname "$(realpath "$0")")"
 
+USERID=$(id -u)
 GROUPID=$(id -g)
 
 echo "Building docker image"
 docker build -t tabbuild .
 
 echo "Building tabfile"
-docker run -v "$(pwd)":/root tabbuild /bin/bash -c "cd /root && make && mv build/tabfile . && rm -rf build && chown ${UID}:${GROUPID} tabfile"
+docker run -v "$(pwd)":/root tabbuild /bin/bash -c "cd /root && make && mv build/tabfile . && rm -rf build && chown ${USERID}:${GROUPID} tabfile"
 
