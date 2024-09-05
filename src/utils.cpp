3bd9e1b8ca8483a864f2e8f69b17ec53a1489a99
j842 <j@842.be>
2024-09-05 14:42:25 +1200

    Fix

--- src/utils.cpp
+++ src/utils.cpp
@@ -44,7 +44,7 @@ watcher::watcher(std::vector<std::string> recursivepaths)
                 if (dir_entry.is_directory())
                     wd.push_back(
                         inotify_add_watch( fd, dir_entry.path().c_str(), 
-                        IN_CLOSE_WRITE | IN_MOVED_TO | IN_MOVE_SELF | IN_DELETE_SELF | IN_DELETE  /*| IN_CREATE | IN_DELETE | IN_MOVED_FROM | IN_MOVED_TO*/ )
+                        IN_CLOSE_WRITE | IN_MOVED_TO | IN_MOVE_SELF | IN_DELETE_SELF | IN_DELETE | IN_CREATE )
                     );
         }
     }
